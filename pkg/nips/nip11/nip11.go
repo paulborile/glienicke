fabd5c052ef51d32fbd285cb8365101edc94d859
paul <paul.borile@gmail.com>
2025-11-21 12:11:49 +0100

    Missing nip11 files in PR

---
+++ pkg/nips/nip11/nip11.go
@@ -0,0 +1,19 @@
+package nip11
+
+import "encoding/json"
+
+// RelayInformationDocument represents the NIP-11 relay information document.
+type RelayInformationDocument struct {
+	Name          string `json:"name,omitempty"`
+	Description   string `json:"description,omitempty"`
+	Pubkey        string `json:"pubkey,omitempty"`
+	Contact       string `json:"contact,omitempty"`
+	SupportedNIPs []int  `json:"supported_nips,omitempty"`
+	Software      string `json:"software,omitempty"`
+	Version       string `json:"version,omitempty"`
+}
+
+// ToJSON returns the JSON encoding of the document.
+func (d *RelayInformationDocument) ToJSON() ([]byte, error) {
+	return json.Marshal(d)
+}
